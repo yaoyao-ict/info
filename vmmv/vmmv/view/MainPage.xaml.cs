107b4dfdf6e50235c546321e1cde395e836b36bd
yaoyao-ict <yaoyao.ict@gmail.com>
2016-04-12 16:28:22 +0800

    Added files via upload

---
+++ vmmv/vmmv/view/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+
+
+namespace vmmv
+{
+    public partial class MainPage : UserControl
+    {
+        private Storyboard myStoryboard;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Binding binding = new Binding("Value");
+            binding.Source = slider;
+            binding.Mode = BindingMode.OneWay;
+            binding.Converter = new converterSlider2TextBox();
+            tb.SetBinding(TextBox.TextProperty, binding);
+
+
+            stulist.ItemsSource = new StudentsVM().Students;
+            stulist.DisplayMemberPath = "Name";
+
+            sId.SetBinding(TextBox.TextProperty, new Binding("SelectedItem.StuNum"){ Source = stulist});
+            sAge.SetBinding(TextBox.TextProperty, new Binding("SelectedItem.Age") { Source = stulist });
+
+
+            DoubleAnimation myDoubleAnimation = new DoubleAnimation();
+            myDoubleAnimation.From = 50.0;
+            myDoubleAnimation.To = 60.0;
+            myDoubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+            myDoubleAnimation.AutoReverse = true;
+            myDoubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            myStoryboard = new Storyboard();
+            myStoryboard.Children.Add(myDoubleAnimation);
+
+            Storyboard.SetTarget(myDoubleAnimation, b);
+            Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(Button.WidthProperty));
+            b.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+            b.Loaded += new RoutedEventHandler(myButtonLoaded);
+        }
+
+        private void myButtonLoaded(object sender, RoutedEventArgs e)
+        {
+            myStoryboard.Begin();
+        }
+    }
+}
---
+++ vmmv/vmmv/viewmodel/StudentsVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace vmmv
+{
+    public class StudentsVM : INotifyPropertyChanged
+    {
+        private List<Student> students;
+        public List<Student> Students
+        {
+            get
+            {
+                return students;
+            }
+            set
+            {
+                students = value;
+                if(PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Students"));
+                }
+            }
+        }
+
+        public StudentsVM()
+        {
+            students = new List<Student>()
+            {
+                new Student() {Age = 20, Name = "Vivian", StuNum = 1},
+                new Student() {Age = 21, Name = "James", StuNum = 2},
+                new Student() {Age = 22, Name = "Henry", StuNum = 5},
+                new Student() {Age = 26, Name = "Jack", StuNum = 13},
+                new Student() {Age = 17, Name = "Mike", StuNum = 23}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
